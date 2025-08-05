af9d997d852612e990c909766286f54de03966f3
= <anilsethy470@gmail.com>
2025-08-05 18:16:43 +0530

    error handling

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const Listing = require("./models/listings.js");
 const path = require("path");
 const methodOverride = require('method-override');
 const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const wrapAsync = require("./utils/wrapAsync.js");
+const listingSchema = require("./schema.js");
+// console.log(listingSchema)
 app.engine("ejs",ejsMate);
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended:true}));
@@ -19,58 +23,79 @@ main().then(()=>{
 async function main(){
     mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
+const validateListing = (req,res,next)=>{
+    let {error} =  listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,error);
+    }else{
+        next();
+    }
+}
 //Home Route
 app.get("/",(req,res)=>{
     res.send("hi i am root");
 });
 //Show Route
-app.get("/listings",async (req,res)=>{
+app.get("/listings",wrapAsync(async (req,res)=>{
     let allListing = await Listing.find({});
+    
      res.render("./listings/index.ejs",{allListing})
-});
+}));
 //New Route
 app.get("/listings/new",(req,res)=>{
     res.render("./listings/new.ejs");
 });
-app.get("/listings/:id",async(req,res,next)=>{
+app.get("/listings/:id",wrapAsync(async(req,res,next)=>{
     let {id} = req.params;
     let showListing = await Listing.findById(id);
+    if(!showListing){
+        throw new ExpressError(404,"page not found");
+    }
     res.render("./listings/show.ejs",{showListing});
-});
+}));
 //Create  Route
-app.post("/listings",async(req,res,next)=>{
-    try{
+app.post("/listings",validateListing,wrapAsync(async(req,res,next)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"send valid data fro listing");
+    }
  let newListing = new Listing (req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-    }catch(err){
-        next(err);
-    }
    
-});
-app.get("/listings/:id/edit",async(req,res)=>{
+}));
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id} = req.params;
     let updateListing = await Listing.findById(id);
     res.render("./listings/edit.ejs",{updateListing})
-});
+}));
 
 //Edit Route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",validateListing,wrapAsync(async(req,res)=>{
     let {id} = req.params;
+    if(!req.body.listing){
+        throw new ExpressError(400,"write valid data for listing");
+    }
   await Listing.findByIdAndUpdate(id, req.body.listing);
   res.redirect("/listings");
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id} = req.params;
-    await Listing.findByIdAndDelete(id);
+    let deleteListing = await Listing.findByIdAndDelete(id);
+    if(!deleteListing){
+        throw ExpressError(400,"Listing not found");
+    }
     res.redirect("/listings");
-   
+}));
+app.use((req,res)=>{
+     throw new ExpressError(404,"Page not found");
 });
 app.use((err,req,res,next)=>{
-    res.send("Something went wrong");
-})
+    let {status=500,message="some error occured"} = err;
+    res.status(status).render("./error.ejs",{err});
+});
 app.listen(8080,()=>{
     console.log("server listing port at 8080");
 })
--- models/listings.js
+++ models/listings.js
@@ -12,6 +12,7 @@ const listingSchema = new Schema({
     image:{
         url:{
             type:String,
+            set: (v)=> v==="" ? "https://images.unsplash.com/photo-1718111990329-cfe23a502e06?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
         },
     },
     price:{
