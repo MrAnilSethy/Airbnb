71697cc7f610a0aa37695dcc576ddf0f6be22749
= <anilsethy470@gmail.com>
2025-08-29 00:01:00 +0530

    added login page completly

--- routes/user.js
+++ routes/user.js
@@ -3,22 +3,34 @@ const router = express.Router();
 const User = require("../models/user.js");
 const ExpressError = require("../utils/ExpressError.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 router.get("/signup",(req,res)=>{
    res.render("users/signup.ejs");
 });
-router.post("/signup",wrapAsync(async(req,res)=>{
+router.post("/signup",async(req,res)=>{
     try{
   let {username,email,password} = req.body;
     let newUser = new User({username,email});
     await User.register(newUser,password);
+    req.flash("success","Welcome to wanderLust");
      res.redirect("/listings");
     }
     catch(e){
         req.flash("error","username already exit");
         console.log(e.message)
         res.redirect("/signup");
-    }
-    
-     
-}));
+    } 
+});
+
+//login
+router.get("/login",(req,res)=>{
+  res.render("users/login.ejs");
+});
+
+router.post("/login",
+  passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}), //passport middleware
+  (req,res)=>{
+    req.flash("success","Welcome to WanderLust");
+    res.redirect("/listings");
+});
 module.exports = router;
